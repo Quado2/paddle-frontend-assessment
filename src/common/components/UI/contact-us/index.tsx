86cdbdfeb00b40dfae743aafcef4184ffaeccf06
Quado2 <engchikwas404@gmail.com>
2022-06-01 23:38:30 +0100

    solved ui bug caused by the contact us page then added the redirects for routing

--- src/common/components/UI/contact-us/index.tsx
+++ src/common/components/UI/contact-us/index.tsx
@@ -1,6 +1,6 @@
 import { BsArrowRight } from "react-icons/bs";
 import { Input, TextArea } from "../";
-import { ContactWrapper,Text} from "./style";
+import { ContactWrapper, Text } from "./style";
 import { useContext } from "react";
 import { AppContext } from "../../../../context/AppContext";
 
@@ -9,36 +9,37 @@ import { inputData } from "./data";
 export interface IContactUsProps {}
 
 export function ContactUs(props: IContactUsProps) {
-
-  const {showContactForm, setShowContactForm} = useContext(AppContext);
-  
+  const { showContactForm, setShowContactForm } = useContext(AppContext);
 
   return (
     <ContactWrapper show={showContactForm}>
-      <div onClick={() => setShowContactForm(false)} className="close">
-        <BsArrowRight className="icon" />
-      </div>
-      <div className="text">
-        <Text>Hey, we are still in the works, but </Text>
-        <Text>you can send us a message!</Text>
-      </div>
-      {inputData &&
-        inputData.map((data, i) => (
-          <Input
-            key = {i}
-            name={data.name}
-            type={data.type}
-            placeholder={data.placeholder}
-            title={data.title} />
-        ))}
+      <div className="container">
+        <div onClick={() => setShowContactForm(false)} className="close">
+          <BsArrowRight className="icon" />
+        </div>
+        <div className="text">
+          <Text>Hey, we are still in the works, </Text>
+          <Text>but you can send us a message!</Text>
+        </div>
+        {inputData &&
+          inputData.map((data, i) => (
+            <Input
+              key={i}
+              name={data.name}
+              type={data.type}
+              placeholder={data.placeholder}
+              title={data.title}
+            />
+          ))}
 
-      <TextArea
-        name="help"
-        title="Tell Us What You Need Help With:"
-        placeholder='Enter A Topic, Like "Channel Problem..." '
-      />
-      <div className="button">
-        <button>SEND NOW</button>
+        <TextArea
+          name="help"
+          title="Tell Us What You Need Help With:"
+          placeholder='Enter A Topic, Like "Channel Problem..." '
+        />
+        <div className="button">
+          <button>SEND NOW</button>
+        </div>
       </div>
     </ContactWrapper>
   );
--- src/common/components/UI/contact-us/style.ts
+++ src/common/components/UI/contact-us/style.ts
@@ -6,95 +6,116 @@ interface ContactProps {
 }
 
 export const ContactWrapper = styled(Flex)<ContactProps>`
-  width: 45.5rem;
+  width: 50.5rem;
+  position: relative;
   transition: ease-in 0.9s all 0.3s;
   z-index: ${({ show }) => (show ? 10 : -1)};
   opacity: ${({ show }) => (show ? 1 : 0)};
-  /* height: ${({ show }) => (show ? "auto" : "100vh")}; */
-  background-image: linear-gradient(45deg, rgb(19, 0, 46), rgb(33, 0, 69));
   position: absolute;
   top: 0;
   right: 0;
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  padding-left: 5rem;
+  max-height: 100vh;
+  overflow-y: scroll;
+  overflow-x: scroll;
+  justify-content: flex-end;
+
+  .container {
+    width: 90%;
+    background-image: linear-gradient(45deg, rgb(19, 0, 46), rgb(33, 0, 69));
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    padding-left: 3rem;
+    padding-top: 17rem;
+
+    @media screen and (max-width: ${({ theme }) => theme.screen.tablet}) {
+      padding-top: 5rem;
+    }
+
+    @media screen and (max-width: ${({ theme }) => theme.screen.phone}) {
+      padding-top: 10rem;
+      padding-left: 0rem;
+      align-items: center;
+    }
+
+    .close {
+      width: 4.9rem;
+      height: 4.9rem;
+      border-radius: 50%;
+      background-color: ${({ theme }) => theme.bg.primary};
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      position: absolute;
+      top: 3.5%;
+      left: 2.5rem;
+      cursor: pointer;
+
+      &:hover {
+        .icon {
+          color: ${({ theme }) => theme.text.active_link};
+        }
+      }
+
+      @media screen and (max-width: ${({ theme }) => theme.screen.tablet}) {
+        left: 1.8rem
+      }
+
+      @media screen and (max-width: ${({ theme }) => theme.screen.phone}) {
+        width: 3rem;
+        height: 3rem;
+        left: .8rem;
+        top: 2.5%;
+      }
+
+      .icon {
+        color: ${({ theme }) => theme.button.primary};
+        font-size: 1.8rem;
+      }
+    }
+
+    .text {
+      margin: 10rem 3rem 2rem 3rem;
+
+      @media screen and (max-width: ${({ theme }) => theme.screen.phone}) {
+        margin: 5rem 0.7rem 2rem 0.7rem;
+      }
+    }
+
+    @keyframes fadeIn {
+      to {
+        z-index: 10;
+        opacity: 1;
+      }
+    }
+
+    .button {
+      padding-bottom: 3rem;
+      button {
+        border: none;
+        outline: none;
+        padding: 1.3rem 3.5rem;
+        border-radius: 2.2rem;
+        background-color: ${({ theme }) => theme.button.primary};
+        color: ${({ theme }) => theme.text.primary};
+        font-weight: 500;
+        font-size: 1.2rem;
+
+        @media screen and (max-width: ${({ theme }) => theme.screen.phone}) {
+          padding: 0.8rem 1.8rem;
+          border-radius: 1.5rem;
+          font-size: 0.8rem;
+        }
+      }
+    }
+  }
 
   @media screen and (max-width: ${({ theme }) => theme.screen.tablet}) {
     width: 90vw;
   }
 
   @media screen and (max-width: ${({ theme }) => theme.screen.phone}) {
-    width: 90vw;
-    padding-left: 0rem;
-    align-items: center;
-  }
-
-  .close {
-    width: 4.9rem;
-    height: 4.9rem;
-    border-radius: 50%;
-    background-color: ${({ theme }) => theme.bg.primary};
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    position: absolute;
-    top: 3.5%;
-    left: -2.25rem;
-    cursor: pointer;
-
-    &:hover {
-      .icon {
-        color: ${({ theme }) => theme.text.active_link};
-      }
-    }
-
-    @media screen and (max-width: ${({ theme }) => theme.screen.phone}) {
-      width: 3rem;
-      height: 3rem;
-      left: -1.5rem;
-      top: 2.5%;
-    }
-
-    .icon {
-      color: ${({ theme }) => theme.button.primary};
-      font-size: 1.8rem;
-    }
-  }
-
-  .text {
-    margin: 10rem 3rem 2rem 3rem;
-
-    @media screen and (max-width: ${({ theme }) => theme.screen.phone}) {
-      margin: 5rem 0.7rem 2rem 0.7rem;
-    }
-  }
-
-  @keyframes fadeIn {
-    to {
-      z-index: 10;
-      opacity: 1;
-    }
-  }
-
-  .button {
-    padding-bottom: 3rem;
-    button {
-      border: none;
-      outline: none;
-      padding: 1.3rem 3.5rem;
-      border-radius: 2.2rem;
-      background-color: ${({ theme }) => theme.button.primary};
-      color: ${({ theme }) => theme.text.primary};
-      font-weight: 500;
-      font-size: 1.2rem;
-
-      @media screen and (max-width: ${({ theme }) => theme.screen.phone}) {
-        padding: 0.8rem 1.8rem;
-        border-radius: 1.5rem;
-        font-size: .8rem;
-      }
-    }
+    width: 100vw;
   }
 `;
 
